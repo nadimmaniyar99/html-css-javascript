0644338379da7407d49feef53ae982997e4a5549
nadimmaniyar99 <nadimmaniyar143@gmail.com>
2024-06-21 20:53:25 +0530

    old assignment added

---
+++ JavaScript/app.js
@@ -0,0 +1,18 @@
+var a =document.querySelector("h1")
+
+console.log(a )
+a.addEventListener("click",function(){
+    a.innerHTML="changed"
+    a.style.background="black"
+    console.log("ghg")
+})
+var button = document.querySelector("button")
+
+
+button.addEventListener("click", function(){
+    console.log("SDADa")
+})
+
+
+var flag=0
+
---
+++ Nike Shoes/Build-and-Deploy-Ecommerce-Website-main/script.js
@@ -0,0 +1,12 @@
+const bar = document.getAnimations('bar');
+const nav =document.getElementById('navbar')
+
+if (bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+
+    if (close){
+        close.addEventListener('click',()=>{
+            nav.classList.remove('active');
+        })
